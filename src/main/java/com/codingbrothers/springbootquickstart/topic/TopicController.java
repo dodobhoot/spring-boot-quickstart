247166b5e2f7d9a86010a10c6ddd2afa59a0bc90
dodobhoot <dbanerjee700@gmail.com>
2019-04-09 03:11:36 +0530

    Added put and delete REST APIs

--- src/main/java/com/codingbrothers/springbootquickstart/topic/TopicController.java
+++ src/main/java/com/codingbrothers/springbootquickstart/topic/TopicController.java
@@ -1,10 +1,8 @@
 package com.codingbrothers.springbootquickstart.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -14,7 +12,28 @@ public class TopicController {
     TopicService topicService;
 
     @RequestMapping("/topics")
-    public List<Topic> getAllTopics(){
+    public List<Topic> getAllTopics() {
         return topicService.getAllTopics();
     }
+
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id) {
+        return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics/")
+    public void addTopic(@RequestBody Topic topic) {
+        topicService.addTopic(topic);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+        topicService.updateTopic(id, topic);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+    public void deleteTopic(@PathVariable String id) {
+        topicService.deleteTopic(id);
+    }
+
 }
--- src/main/java/com/codingbrothers/springbootquickstart/topic/TopicService.java
+++ src/main/java/com/codingbrothers/springbootquickstart/topic/TopicService.java
@@ -2,18 +2,46 @@ package com.codingbrothers.springbootquickstart.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
-    private List<Topic> topics = Arrays.asList(
+    private List<Topic> topics = new ArrayList<Topic>(Arrays.asList(
             new Topic("Spring", "Spring Framework", "Spring Framework Description"),
             new Topic("Java", "Core Java", "Core Java Description"),
             new Topic("Javascript", "Javascript", "Javascript Description")
-    );
+    ));
 
-    public List<Topic> getAllTopics(){
+    public List<Topic> getAllTopics() {
         return topics;
+    }
+
+    public Topic getTopic(String id) {
+        for (Topic topic : topics) {
+            if (topic.getId().equals(id)) {
+                return topic;
+            }
+        }
+        return null;
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+        for (int i = 0; i < topics.size(); i++) {
+            if (topics.get(i).getId().equals(topic.getId()))
+                topics.set(i, topic);
+        }
+    }
+
+    public void deleteTopic(String id) {
+        for (int i = 0; i < topics.size(); i++) {
+            if (topics.get(i).getId().equals(id))
+                topics.remove(i);
+        }
     }
 }
